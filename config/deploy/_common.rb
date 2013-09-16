0b66d3bf294996de7476998a6ba37db9de0b2de0
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-16 22:26:37 +0900

    fix

--- config/deploy/_common.rb
+++ config/deploy/_common.rb
@@ -7,12 +7,32 @@ set :unicorn_config, "#{current_path}/config/unicorn/#{stage}.rb"
 set :unicorn_pid, "#{current_path}/tmp/pids/unicorn.pid"
 
 before 'deploy:setup', 'rvm:install_ruby'
-after 'deploy:restart', :oguni,'unicorn:restart'
+after 'deploy:restart', :oguni#,'unicorn:restart'
 
 task :oguni do
   run "logger oguni task start"
   #run "cp -a  /home/deploy/#{application}/#{stage}/shared/production/database.yml #{current_path}/config/database.yml"    
 end
+namespace :deploy do
+  task :start, :roles => :app do
+    run "cd #{current_path}; bundle exec unicorn_rails -c config/unicorn.rb -E #{rails_env} -D"
+  end
+  task :restart, :roles => :app do
+    run "echo #{current_path}/tmp/pids/unicorn.pid"
+    path = "#{current_path}/tmp/pids/unicorn.pid"
+    run "cat #{path}"
+
+    if File.exist? "#{current_path}/tmp/pids/unicorn.pid"
+      run "kill -s USR2 `cat #{current_path}/tmp/unicorn_#{application}.pid`"
+    else 
+      run "echo not found"
+    end
+  end
+  task :stop, :roles => :app do
+    run "kill -s QUIT `cat t#{current_path}/tmp/unicorn.pid`"
+  end
+end
+=begin
 namespace :deploy do
   task :restart, :except => { :no_release => true } do
     run "logger restart task start"
@@ -26,3 +46,4 @@ namespace :deploy do
   #run "mv -f #{latest_release}/config.yml.prod #{latest_release}/config.yml"
   #end
 end
+=end
