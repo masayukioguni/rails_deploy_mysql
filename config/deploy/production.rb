8b1add8141fcc3a2e332151c588e08ee853761ee
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-17 11:19:56 +0900

    fix production env

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
 load 'deploy/assets'
 load 'config/deploy/_common'
 
+default_environment['MYSQL_PASSWORD'] = ENV['RAILS_TEST_PASSWORD']
+default_environment['MYSQL_USERNAME'] = ENV['RAILS_TEST_USERNAME']
