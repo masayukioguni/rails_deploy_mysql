cf44fef5c44bfc7c45baae71963546543023e24d
masayuki_oguni <masayuki_oguni.dev@gmail.com>
2013-09-17 11:26:20 +0900

    fix db name

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,5 @@ load 'config/deploy/_common'
 
 default_environment['MYSQL_PASSWORD'] = ENV['RAILS_TEST_PASSWORD']
 default_environment['MYSQL_USERNAME'] = ENV['RAILS_TEST_USERNAME']
+default_environment['MYSQL_DB_NAME'] = ENV['RAILS_TEST_DB_NAME']
+
